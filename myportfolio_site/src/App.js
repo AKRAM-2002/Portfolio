ae8460524138a22a0aeb4137fa866311e59e9109
Akram-2002 <Akrambusiness6@gmail.com>
2023-07-09 22:10:57 +0100

    push components

---
+++ myportfolio_site/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import About from './components/About';
+import Skills from './components/Skills';
+import Work from './components/Work';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Home/>
+      <About/>
+      <Skills/>
+      <Work/>
+    </div>
+  );
+}
+
+export default App;
